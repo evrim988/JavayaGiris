82b3e9c2bb1c5a01aac32e1cb1dfa229315a0d50
User <evrim98@outlook.com>
2024-07-10 09:08:39 +0300

    diziler ve örnekler

---
+++ src/main/java/org/diziler/DiziTekrar/CokBoyutluDiziler.java
@@ -0,0 +1,13 @@
+package org.diziler.DiziTekrar;
+
+public class CokBoyutluDiziler {
+    public static void main(String[] args) {
+
+        int[] sayilar = {1,2,3,4,5}; //tek boyutlu dizi
+
+        int[][] koordinatlar = {
+                {1,2,3},
+                {3,4,5},
+        };
+    }
+}
